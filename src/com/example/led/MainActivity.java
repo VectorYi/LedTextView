e163ea60997ba3096dcbc1dcb9f402607bc1eb40
Vector_Yi <family686993@hotmail.com>
2015-03-10 10:02:24 +0800

    Init project

---
+++ src/com/example/led/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.led;
+
+import com.example.ledtextview.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
